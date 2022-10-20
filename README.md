b4c8414e65bcc71ca177c7332682775a25318446
Jacques BONET <jacques.bonet@gmail.com>
2022-10-20 16:08:10 +0200

    First release

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# r_sample
+# Exercice
+
+Solve the following question using RScript, part of the R programming language.  We do not expect you to currently know any R, so before your interview please learn enough R to use it to write an answer to the following question.
+
+
+Bring your completed answer along to your interview ready to demonstrate - you will be expected to explain it and modify it a little during the interview.  You will have access to a web browser / Google as you are doing this.
+
+
+Given a CSV file of the form:
+
+
+operationtype,starttime,endtime
+
+find,1,5
+
+find,3,8
+
+delete,2,9
+
+find,4,6
+
+update,1,8
+
+delete,7,11
+
+
+please output the Max, Min, Mean and Median duration for each operation type using RScript. Times are in seconds. Feel free to add more rows or operation types to the sample data set above.
+
+
+# Result
+
+If I don't put some print in the program, the program crash, very curious.
---
+++ main.R
@@ -0,0 +1,56 @@
+setClass("duration", slots=list(find="numeric", delete="numeric", update="numeric"))
+setClass("medDuration", slots=list(findMed="numeric", deleteMed="numeric", updateMed="numeric", find="list", delete="list", update="list"))
+
+initDuration <- new("duration",find=0, delete=0, update=0)
+initMedDuration <- new("medDuration",findMed=0, deleteMed=0, updateMed=0, find=list(), delete=list(), update=list())
+dataToProcess <- read.csv(file = 'data.csv', header = TRUE)
+
+initDuration@find = 1
+
+maxReducer <- function(acc, idx) {
+  duration = strtoi(dataToProcess[idx, 3]) - strtoi(dataToProcess[idx, 2])
+  if (duration > slot(acc, dataToProcess[idx, 1])) {
+    slot(acc, dataToProcess[idx, 1]) = duration
+  }
+  print(acc)
+}
+
+minReducer <- function(acc, idx) {
+  duration = strtoi(dataToProcess[idx, 3]) - strtoi(dataToProcess[idx, 2])
+  if (duration < slot(acc, dataToProcess[idx, 1]) || slot(acc, dataToProcess[idx, 1]) == 0) {
+    slot(acc, dataToProcess[idx, 1]) = duration
+  }
+  print(acc)
+}
+meanReducer <- function(acc, idx) {
+  duration = strtoi(dataToProcess[idx, 3]) - strtoi(dataToProcess[idx, 2])
+  slot(acc, dataToProcess[idx, 1]) = slot(acc, dataToProcess[idx, 1]) + duration
+  if (idx == nrow(dataToProcess)) {
+    acc@find = acc@find / sum(dataToProcess$operationtype == "find")
+    acc@delete = acc@delete / sum(dataToProcess$operationtype == "delete")
+    acc@update = acc@update / sum(dataToProcess$operationtype == "update")
+  }
+  print(acc)
+}
+medReducer <- function(acc, idx) {
+  duration = strtoi(dataToProcess[idx, 3]) - strtoi(dataToProcess[idx, 2])
+  slot(acc, dataToProcess[idx, 1]) = append(slot(acc, dataToProcess[idx, 1]), duration)
+  if (idx == nrow(dataToProcess)) {
+    acc@findMed = median(sort(unlist(acc@find)))
+    acc@deleteMed = median(sort(unlist(acc@delete)))
+    acc@updateMed = median(sort(unlist(acc@update)))
+  }
+  print(acc)
+}
+
+print("======================= MAX DURATION =======================================")
+print(Reduce( maxReducer, 1:nrow(dataToProcess), initDuration))
+print("======================= MIN DURATION =======================================")
+print(Reduce( minReducer, 1:nrow(dataToProcess), initDuration))
+print("======================= MEAN DURATION =======================================")
+print(Reduce( meanReducer, 1:nrow(dataToProcess), initDuration))
+print("======================= MED DURATION =======================================")
+print(Reduce( medReducer, 1:nrow(dataToProcess), initMedDuration))
+
+
+
